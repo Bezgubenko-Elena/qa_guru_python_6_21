9dd6c24c9110742eb94b5b0bc05c4ea0ab7e801f
Bezgubenko Helen <protasova-ea@yandex.ru>
2023-09-09 23:59:31 +0300

    done tests

---
+++ tests/test_catfact.py
@@ -0,0 +1,33 @@
+from jsonschema.validators import validate
+import json
+import os
+from utils import helper
+
+path_schema = os.path.abspath(os.path.join(os.path.dirname(__file__), 'resources'))
+base_url = "https://catfact.ninja"
+
+
+def test_get_list_breeds():
+    response = helper.catfact_api('get', '/breeds')
+
+    assert response.status_code == 200
+
+
+def test_get_list_breeds_scheema():
+    with open(os.path.join(path_schema, "schema_breeds.json")) as file:
+        schema = json.loads(file.read())
+        response = helper.catfact_api('get', '/breeds')
+
+        validate(instance=response.json(), schema=schema)
+
+
+def test_get_random_fact():
+    response = helper.catfact_api('get', '/fact')
+
+    assert response.status_code == 200
+
+
+def test_get_random_facts():
+    response = helper.catfact_api(method='get', url='/fact')
+
+    assert response.status_code == 200
